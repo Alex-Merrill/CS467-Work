bec1f7172e2f20a8dcc5758ac873590083d88f21
Alex-Merrill <aomerrill98@gmail.com>
2020-03-24 01:45:14 -0400

    test2

---
+++ PracticeSketches/sketches.php
@@ -0,0 +1,38 @@
+<?php
+
+// $paths = array('Assignments', 'Practicals', 'PracticeSketches');
+//
+// $files1 = scandir($paths[0]);
+// $files2 = scandir($paths[1]);
+// $files3 = scandir($paths[2]);
+//
+// $allFiles = array($files1, $files2, $files3);
+// $iter = 0;
+
+$files = scandir('.');
+
+//foreach($allFiles as $files) {
+	foreach ($files as $f){
+		$name = join(DIRECTORY_SEPARATOR, [getcwd(), $f]);
+
+		// note that we multiply times by 1000 because PHP is in seconds
+		// others expect milliseconds
+		if ($f[0] != '.' && is_dir($name)){
+		   $dirs[] = (object) [
+			'name' => $name,
+			'ctime' => stat($name)['ctime'] * 1000,
+			'mtime' => stat($name)['mtime'] * 1000
+			];
+		}
+	}
+	//$iter += 1;
+//}
+
+echo json_encode($dirs);
+
+
+
+
+
+
+?>
