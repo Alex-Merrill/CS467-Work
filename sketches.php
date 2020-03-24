a682d7504f5fad07d717c806bd417d9113d75fe0
Alex-Merrill <aomerrill98@gmail.com>
2020-03-24 01:24:18 -0400

    5.5

--- sketches.php
+++ sketches.php
@@ -6,11 +6,11 @@ $files1 = scandir($paths[0]);
 $files2 = scandir($paths[1]);
 $files3 = scandir($paths[2]);
 
-$dirs = array($files1, $files2, $files3);
-
-foreach($dirs as $files) {
+$allFiles = array($files1, $files2, $files3);
+$iter = 0;
+foreach($allFiles as $files) {
 	foreach ($files as $f){
-		$name = join(DIRECTORY_SEPARATOR, [$path, $f]);
+		$name = join(DIRECTORY_SEPARATOR, [$paths[$iter], $f]);
 
 		// note that we multiply times by 1000 because PHP is in seconds
 		// others expect milliseconds
@@ -22,6 +22,7 @@ foreach($dirs as $files) {
 			];
 		}
 	}
+	$iter += 1;
 }
 
 echo json_encode($dirs);
